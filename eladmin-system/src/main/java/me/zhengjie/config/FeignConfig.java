51ea117c722806d55b6768f4747f9f67db6eb072
Aroli <yanhuakang@iandun.cn>
2020-04-07 20:31:24 +0800

    修改feign配置

--- eladmin-system/src/main/java/me/zhengjie/config/FeignConfig.java
+++ eladmin-system/src/main/java/me/zhengjie/config/FeignConfig.java
@@ -4,12 +4,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import feign.Logger;
+import feign.Request;
 
 @Configuration
 public class FeignConfig {
 
-	@Bean  
-    Logger.Level feignLoggerLevel() {  
-        return Logger.Level.FULL;  
-    }  
+	@Bean
+	Logger.Level feignLoggerLevel() {
+		return Logger.Level.FULL;
+	}
+
+	@Bean
+	Request.Options feignOptions() {
+		return new Request.Options(60 * 1000, 60 * 1000);
+	}
 }
